6f9093e55fbad5b1ebc4bb63425b3f71be94d210
pave-botowner <garlaschini.davide@gmail.com>
2026-09-23 19:28:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const express = require('express?);
-                        const app = express();
-app.get('\', (req, res)c => res.send('PAVE-BOT ON'));
-    app.listen(process.env.PORT|| 3000, () => console.log('server web ON'));
-process.on('uncaughtException', e => console.log('CRASH:', e))
-process.on('unhandledRejection', e => console.log('CRASH PROMISE:', e))
-
-const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys')
+const { default: makeWASocket,useMultiFileAuthState } =
+  require('@whiskeysockets/baileys')
 const pino = require('pino')
-const fs = require('fs')
-const express = require('express')
-
+const fs = require ('fs')
+const express = require ('express')
 const app = express()
-app.get('/', (req,res)=>res.send('PAVE-BOT ON - ' + new Date().toISOString()))
-const PORT = process.env.PORT || 3000
-app.listen(PORT, '0.0.0.0', ()=>console.log('WEB SERVER ON PORT ' + PORT))
+app.get('/', (req, res) => (res.send('BOT ON' + new Date().toISOString())
+})
+const PORT = process.env.PORT|| 3000
+app.listener(PORT, '0.0.0.0',() => console.log('WEB SERVER ON PORT ' + PORT))
 
 const BOT_NAME = "PAVE-BOT"
 const OWNER_ID = "393381532143"
