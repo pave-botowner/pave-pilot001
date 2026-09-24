b5f9135798604f2549820c9a11de2dc3358b2efa
pave-botowner <garlaschini.davide@gmail.com>
2026-09-24 19:23:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const col=mongoose.connection.db.collection('auth')
       let creds=await r('creds')||initAuthCreds()
       let keys={}
       const saveCreds=async()=>{await w(creds, 'creds')}
-      const state{creds, keys:{get.async(t, ids)=>{let o={};for(let i of ids){let k= '${t}-${i}';if(keys[k])
+      const state={creds, keys:{get:async(t, ids)=>{let o={};for(let i of ids){let k= '${t}-${i}';if(keys[k])
         o[i]=d}}}return o},set:async(d)=>{for(let c in d) 
       {for(let i in d[c]) {let k= '${c}-${i}';keys[k]=d[c][i];await w (d[c][i],k)}}
                                                                                                                                                 saveCreds()}}}
