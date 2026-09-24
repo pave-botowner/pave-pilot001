5203c8622bcd824e57148c7b0106d7644ecd9cc6
pave-botowner <garlaschini.davide@gmail.com>
2026-09-24 19:42:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const col=mongoose.connection.db.collection('auth')
                     }
                 }
 if (lower === '/menu') {await 
-                        sock.sendMessage(chatJid, {text:'*_💵🤖PAVE-BOT🤖💵_*\n\n/menu -> questo menu\n' })
+                        sock.sendMessage(chatJid, {text:'*_💵🤖PAVE-BOT🤖💵_*\n\n/menu -> questo menu\n' }}
     return
                        }
     } catch (err) { console.log('ERRORE MSG:', err) }
